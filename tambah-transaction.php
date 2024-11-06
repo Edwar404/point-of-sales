b277924483d796e0a3a5e3e5bffd66efb7c7bb7b
Edwar404 <edwar.ibrahim1813336@gmail.com>
2024-11-06 15:22:35 +0200

    penambahan fitur baru Javascript

--- tambah-transaction.php
+++ tambah-transaction.php
@@ -8,11 +8,15 @@ require_once 'config/koneksi.php';
 $currentTime = date('Y-m-d');
 // $currentTime = date('Y-m-d H:i:s');
 
-function generateTransactionCode() {
-    // $query = "SELECT MAX(RIGHT(kode_transaksi, 6)) as kode_max FROM transaksi WHERE DATE(tanggal_transaksi) = CURDATE()";
-    // $result = mysqli_query($conn, $query);
-    // $data = mysqli_fetch_assoc($result);
-    // $kode_max = $data['kode_max'];
+function generateTransactionCode()
+{
+    $kode = date('ymdHis');
+    return $kode;
+}
+
+// CLICK COUNT
+if (empty($_SESSION['click_count'])) {
+    $_SESSION['click_count'] = 0;
 }
 ?>
 
@@ -38,15 +42,52 @@ function generateTransactionCode() {
                 <form action="" method="POST">
                     <div class="mb-2">
                         <label class="form-label" for="">Kode Transaksi</label>
-                        <input type="text" id="kode_transaksi" value="<?php echo 'TR-' . generateTransactionCode()?>" name="kode_transaksi" class="form-control w-50" readonly>
+                        <input type="text" id="kode_transaksi" value="<?php echo 'TR-' . generateTransactionCode() ?>" name="kode_transaksi" class="form-control w-50" readonly>
                     </div>
                     <div class="mb-2">
                         <label class="form-label" for="">Tanggal</label>
                         <input type="date" name="tanggal_transaksi" value="<?php echo $currentTime ?>" id="tanggal_transaksi" class="form-control w-50" readonly>
                     </div>
-                    <div class="mt-4">
+                    <div class="my-4">
                         <button class="btn btn-primary btn-sm" type="button" id="counterBtn">Tambah</button>
-                        <input type="number" style="width: 465px;" name="countDisplay" id="countDisplay" readonly>
+                        <input type="number" style="width: 465px;" name="countDisplay" value="<?php echo $_SESSION['click_count'] ?>" id="countDisplay" readonly>
+                    </div>
+                    <div class="table table-responsive">
+                        <table class="table table-bordered">
+                            <thead class="text-center">
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Nama Kategori</th>
+                                    <th>Nama Barang</th>
+                                    <th>jumlah</th>
+                                    <th>Sisa Produk</th>
+                                    <th>Harga</th>
+                                </tr>
+                            </thead>
+                            <tbody id="tbody">
+                                <!-- DATA DITAMBAH DI SINI -->
+
+                            </tbody>
+                            <tfoot class="text-center">
+                                <tr>
+                                    <th colspan="5">Total Harga</th>
+                                    <td><input type="number" id="total_harga_keseluruhan" name="total_harga" class="form-control" readonly></td>
+                                </tr>
+                                <tr>
+                                    <th colspan="5">Nominal Bayar</th>
+                                    <td><input type="number" id="nominal_bayar_keseluruhan" name="nominal_bayar" class="form-control" required></td>
+                                </tr>
+                                <tr>
+                                    <th colspan="5">Kembalian</th>
+                                    <td><input type="number" id="kembalian_keseluruhan" name="kembalian" class="form-control" readonly></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <br><br>
+                    <div class="mb-3">
+                        <input type="submit" class="btn btn-primary" name="simpan" value="Hitung">
+                        <a href="kasir.php" class="btn btn-danger">Kembali</a>
                     </div>
                 </form>
             </div>
@@ -54,7 +95,36 @@ function generateTransactionCode() {
         </div>
     </div>
 
+
+    <?php
+    $queryCategories = mysqli_query($koneksi, "SELECT * FROM kategori_barang");
+    $categories = mysqli_fetch_all($queryCategories, MYSQLI_ASSOC);
+    ?>
     <script src="bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const button = document.getElementById('counterBtn');
+            const countDisplay = document.getElementById('countDisplay');
+            const tbody = document.getElementById('tbody');
+
+            button.addEventListener('click', function() {
+                let currentCount = parseInt(countDisplay.value) || 0;
+                currentCount++;
+                countDisplay.value = currentCount;
+                // FUNGSI TAMBAH TD
+                let newRow = "<tr>";
+                newRow += "<td>" + currentCount + "</td>";
+                newRow += "<td><select class='form-control category-select' name='id_kategori[]' required>";
+                newRow += "<option value=''>--Pilih Kategori--</option>"
+                <?php foreach ($categories as $category) { ?>
+                    newRow += "<option value='<?php echo $category['id']?>'><?php echo $category['nama_kategori']?></option>";
+                <?php } ?>
+                newRow += "</select></td>";
+                newRow += "</tr>";
+                tbody.insertAdjacentHTML('beforeend', newRow)
+            })
+        })
+    </script>
 </body>
 
 </html>
